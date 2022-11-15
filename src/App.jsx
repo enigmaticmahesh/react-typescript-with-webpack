86344b5b035401b12ac9642bfa81db8e709157bc
enigmaticmahesh <challengermahesh@gmail.com>
2022-11-15 22:55:50 +0530

    Implemented React with Webpack

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+const App = () => {
+  return <>Welcome to React</>;
+};
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -11,4 +11,18 @@ module.exports = {
     },
   },
   mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.jsx', '.js'],
+  },
 };
